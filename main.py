cc5ae7dcfd13bf8cbdda460ac02db59cd4617b0b
Will <20180729@aloe.ulima.edu.pe>
2022-11-19 16:29:08 +0000

    cambios

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def my_map():
     return render_template('prueba_mapa.html')
 
+# commit nuevo
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
