6625b5641e05574a668966fe79cc412c0868572a
clausula_martinez18 <clausula.martinez18@gmail.com>
2022-11-19 16:13:35 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_map():
+    return render_template('prueba_mapa.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
